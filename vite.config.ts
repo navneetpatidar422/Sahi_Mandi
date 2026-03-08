0f41948167b18b8a380877056b2d4f0bf9af64da
Navneet Patidar <navneetpatidar422@gmail.com>
2026-03-08 11:47:44 +0530

    base error

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-// Important for GitHub Pages
-base: "/Sahi_Mandi/",
+
 
 plugins: [react()],
 
